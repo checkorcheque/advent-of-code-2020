05cd85ad0d2e0a81a162cbca8aa2b56b06c1454e
checkorcheque <69222936+checkorcheque@users.noreply.github.com>
2020-12-18 23:12:29 -0500

    Add day1 part2 and fix day1 part1

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -1,6 +1,6 @@
-#find the two entries that sum to 2020 and then multiply those two numbers together.
+# find the two entries that sum to 2020 and then multiply those two numbers together.
 
-#turn .txt into a list of nums
+# turn .txt into a list of nums
 with open("day1input_part1.txt", "r") as day1_file:
     inputs = [int(num) for num in day1_file]
 
@@ -8,6 +8,6 @@ for num1 in inputs:
     for num2 in inputs:
         if num1 + num2 == 2020:
             print(f"num1 is {num1} and num2 is {num2}")
-            print(f"num1 * num2 is {num1 * num2}")
+            print(f"the product of these numbers is {num1 * num2}")
 
 
---
+++ day1/day1_part2.py
@@ -0,0 +1,12 @@
+# what is the product of the three entries that sum to 2020?
+# turn .txt into a list of nums
+with open("day1input.txt", "r") as day1_file:
+    inputs = [int(num) for num in day1_file]
+
+for num1 in inputs:
+    for num2 in inputs:
+        for num3 in inputs:
+            if num1 + num2 + num3 == 2020:
+                product(num1 * num2 * num3)
+
+print(product)
